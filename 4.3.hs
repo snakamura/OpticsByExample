5b44921e57c676193bdbebe0dfd64b3144860524
Satoshi Nakamura <snak@snak.org>
2023-05-27 21:46:49 +0900

    Add 4.3

---
+++ 4.3.hs
@@ -0,0 +1,29 @@
+import Control.Lens
+
+-- 1
+a1 = view (_2 . _1 . _2) ("Ginerva", (("Galileo", "Waldo"), "Malfoy"))
+
+{-
+
+2.
+Lens' Eight Two
+
+3.
+Lens Platypus BabySloth Armadillo Hedgehog
+
+pre-action structure: Platypus
+post-action structure: BobySloth
+pre-action focus: Armadillo
+post-action focus: Hedgehog
+
+4.
+  snajubjumwock
+. boowockugwup
+. gruggazinkoom
+. zinkattumblezz
+. spuzorktrowmble
+. gazorlglesnatchka
+. banderyakoobog
+
+
+-}
